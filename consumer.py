40b928957e8bb53225077adc2f86fe40f547b204
Aman Sagar <09amansagar@gmail.com>
2021-09-04 22:28:04 +0530

    consumer.py file updated

--- consumer.py
+++ consumer.py
@@ -1,3 +1,17 @@
 from kafka import KafkaConsumer
-TOPIC_Name=" "
-consumer = KafkaConsumer()
+from json import loads
+import json
+
+KAFKA_TOPIC_NAME_CONS="p3_topic"
+
+consumer = KafkaConsumer(
+    "p3_topic",
+    bootstrap_servers['localhost:9092'],
+    auto_offset_rest=beginning,
+    enable_auto_commit=True,
+    
+        
+        )
+
+for message in consumer:
+    print(message )
