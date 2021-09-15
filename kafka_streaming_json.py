29aa76834405dfc10fdfd88aacf1d13b3d6e5a37
Chintan <munjanichintan@gmail.com>
2021-09-15 16:52:45 +0530

    just push

--- kafka_streaming_json.py
+++ kafka_streaming_json.py
@@ -7,7 +7,7 @@ import time
 
 kafka_topic_name = "p3-topic"
 kafka_bootstrap_servers = 'localhost:9092'
-card_topic_name = 'card_topic'
+card_topic_name = 'card1'
 
 if __name__ == "__main__":
     print("Stream Data Processing Application Started ...")
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    # Construct a streaming DataFrame that reads from test-topic
+    # Construct a streaming DataFrame that reads from p3-topic
     orders_df = spark.readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     print('*****Streaming schema*****')
     orders_df.printSchema()
 
-    orders_df1 = orders_df.withWatermark("timestamp", "1 minutes").selectExpr("CAST(value AS STRING)")
+    orders_df1 = orders_df.selectExpr("CAST(value AS STRING)")
 
     schema = StructType() \
         .add('order_id', IntegerType()) \
@@ -45,8 +45,8 @@ if __name__ == "__main__":
         .add('product_category', StringType()) \
         .add('payment_type', StringType()) \
         .add('qty', IntegerType()) \
-        .add('price', FloatType()) \
-        .add('order_datetime', StringType()) \
+        .add('price', IntegerType()) \
+        .add('order_datetime', TimestampType()) \
         .add('ecommerce_website_name', StringType()) \
         .add('payment_txn_id', StringType()) \
         .add('payment_txn_status', StringType()) \
@@ -64,24 +64,81 @@ if __name__ == "__main__":
     #     .outputMode("append") \
     #     .start()
 
-    od_df1 = orders_df2.groupBy('city', 'payment_type') \
-        .agg(count('order_id').alias("total_order"), sum(col('qty')*col('price')).alias("total_amount"))
+    od_card = orders_df2.filter("payment_type=='Card'")
 
-    od_df1.printSchema()
+    od_card1 = od_card.withColumn("key", lit(100)) \
+                        .withColumn("value", concat(lit("{'order_id': '"), col('order_id').cast("string"),
+                                                    lit("', 'customer_id': '"), col('customer_id').cast("string"),
+                                                    lit("', 'customer_name': '"), col('customer_name'),
+                                                    lit("', 'country': '"), col('country'),
+                                                    lit("', 'city': '"), col('city'),
+                                                    lit("', 'product_id': '"), col('product_id').cast("string"),
+                                                    lit("', 'product_name': '"), col('product_name'),
+                                                    lit("', 'product_category': '"), col('product_category'),
+                                                    lit("', 'payment_type': '"), col('payment_type'),
+                                                    lit("', 'qty': '"), col('qty').cast("string"),
+                                                    lit("', 'price': '"), col('price').cast("string"),
+                                                    lit("', 'order_datetime': '"), col('order_datetime'),
+                                                    lit("', 'ecommerce_website_name': '"), col('ecommerce_website_name'),
+                                                    lit("', 'payment_txn_id': '"), col('payment_txn_id'),
+                                                    lit("', 'payment_txn_status': '"), col('payment_txn_status'),
+                                                    lit("', 'failure_reason': '"), col('failure_reason'), lit("'}")
+                                                    ))
 
-    # od_df = od_df1.writeStream \
-    #     .format('parquet') \
-    #     .outputMode('append') \
-    #     .option('path', '/p3') \
-    #     .trigger(processingTime='20 seconds') \
-    #     .start()
-
-    od_df = od_df1.writeStream \
-        .trigger(processingTime='20 seconds')\
+    trans_detail_write_stream_1 = od_card1 \
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+        .option("topic", card_topic_name) \
+        .trigger(processingTime='1 seconds') \
         .outputMode("update") \
-        .format("console") \
+        .option("checkpointLocation", "file:///home/hdoop/tmp/py_checkpoint") \
         .start()
 
+    # od_card1.printSchema()
+
+    # trans_detail_write_stream = od_card1 \
+    #     .writeStream \
+    #     .trigger(processingTime='10 seconds') \
+    #     .outputMode("update") \
+    #     .option("truncate", "false")\
+    #     .format("console") \
+    #     .start()
+
+    # trans_detail_write_stream.awaitTermination()
+
+    od_parquet = orders_df2.withWatermark("order_datetime", "10 minutes") \
+        .groupBy("city", "payment_type", window("order_datetime", "10 minutes")) \
+        .agg(sum(col('qty')*col('price')).alias('total_amount'), \
+        count('order_id').alias('total_orders')) \
+        .select('city', 'payment_type', 'total_amount', 'total_orders')
+
+    # od_df1 = orders_df2.groupBy('city', 'payment_type') \
+    #     .agg(count('order_id').alias("total_order"), sum(col('qty')*col('price')).alias("total_amount"))
+
+    # od_df1.printSchema()
+
+    od_df = od_parquet.writeStream \
+        .format("console") \
+        .outputMode("append") \
+        .start()
+
+    od_parquet1 = od_parquet.writeStream \
+        .format('parquet') \
+        .outputMode('append') \
+        .option('path', 'p3/output') \
+        .trigger(processingTime='10 minutes') \
+        .option("checkpointLocation", "file:///home/hdoop/tmp/parquet_checkpoint") \
+        .start()
+
+    # od_df = od_df1.writeStream \
+    #     .trigger(processingTime='20 seconds')\
+    #     .outputMode("update") \
+    #     .format("console") \
+    #     .start()
+
+    od_parquet1.awaitTermination()
     od_df.awaitTermination()
 
     # transaction_details = od_df.select('order_id', 'city', (col('qty')*(col('price'))).alias('total_price'))
@@ -121,4 +178,3 @@ if __name__ == "__main__":
     # od_df2.awaitTermination()
     # td_df1.awaitTermination()
     # card_df1.awaitTermination()
-    
